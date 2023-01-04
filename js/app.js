15fb821e515d1835f2e0461d223fdff9763ba00e
AbhaySingh <tan63453@gmail.com>
2023-01-05 00:16:57 +0530

    js

---
+++ js/app.js
@@ -0,0 +1,12 @@
+console.log('hello from app.js')
+let fname = 'Abhay';
+let lname = 'Singh';
+let fullname = `Mr. ${fname} ${lname}`
+console.log(fullname);
+var a = 100;
+b();
+function b() {
+    var a = 9;
+    console.log(a);
+}
+console.log(a);
